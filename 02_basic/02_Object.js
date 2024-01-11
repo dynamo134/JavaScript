2cf273beaf7ad45b00c5b3689ed5b2a1afb9d403
dynamo134 <shaharyaramu16@gmail.com>
2024-01-12 04:40:04 +0530

    Object

---
+++ 02_basic/02_Object.js
@@ -0,0 +1,38 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "shaharyar",
+    "full name": "shaharyar alam",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "aligarh",
+    email: "shaharyar@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "alam@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "alam@microsoft.com"
+// console.log(JsUser);  //will not changed  
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
