860313fb131a7f4ad5f408a367ce46f616a51133
dynamo134 <shaharyaramu16@gmail.com>
2024-01-11 20:32:39 +0530

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -26,3 +26,15 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber)
+
+
+/* *****************************************Operation*************************************************** */
+
+let str1 = "hello"
+let str2 = "shaharyar"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2) //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
