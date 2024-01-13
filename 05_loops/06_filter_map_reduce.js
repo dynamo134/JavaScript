641d9696cdc2579821571adde02aea2e89e83c74
dynamo134 <shaharyaramu16@gmail.com>
2024-01-13 18:10:50 +0530

    filter+map

---
+++ 05_loops/06_filter_map_reduce.js
@@ -0,0 +1,77 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+const values = coding.forEach( (item) => {  //forEach did not return any value 
+    //console.log(item);
+    return item
+} )
+
+console.log(values);  //undefined
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => {
+    return num > 4
+} )
+
+console.log(newNums);  //5,6,7,8,9,10
+
+// const newNums = []
+
+//--------- second way 
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);  //5,6,7,8,9,10
+
+//***********************************************FILTER*************************************** */
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+
+//***********************************************MAP****************************************************** */
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+
+//map chaining
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
+
+// map Method:
+
+// The map method is used to create a new array by applying a provided function to each element in the original array.
+// It returns a new array of the same length as the original array, where each element is the result of applying the provided function to the corresponding element of the original array.
+// The original array remains unchanged.
+
+// filter Method:
+
+// The filter method is used to create a new array with elements that satisfy a provided condition.
+// It returns a new array containing only the elements for which the provided function returns true.
+// The original array remains unchanged.
+  
