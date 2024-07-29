3aa47f561792d5468622a70c1e56af4e7cecc661
dynamo134 <shaharyaramu16@gmail.com>
2024-07-29 16:42:12 +0530

    Data Types

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,20 +1,39 @@
 "use strict"; // treat all JS code as newer version
 
-//alert(3+3)  //we are using Nodejs not Browser
 
+/* Primitive Data Types
+The predefined data types provided by JavaScript language are known as primitive data types. Primitive data types are also known as in-built data types.
+
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+*/
+
+/*
+Non-Primitive Data Types:
+The data types that are derived from primitive data types of the JavaScript language are known as non-primitive data types. It is also known as derived data types or reference data types.
+
+Object
+*/
 
 
 //number => 2 to the power of 53
-//bigInt => 
+//bigInt =>
 //string => ""
-//boolean => 0 or 1 
+//boolean => 0 or 1
 //null =>standaloon value  --> basically a empty value
 //undefined => value not assign yet
 //symbol => use to find uniqness
 
-//Object 
+//Object
 
+let accountEmail = "shaharyar@gmail.com"
+ 
 console.log(typeof "shaharyar")     //-------->String 
 console.log(typeof null)     //--------->Object
-console.log(typeof undefined)
+console.log(typeof undefined)//undefined
 console.log(typeof(accountEmail))   //--------->String
