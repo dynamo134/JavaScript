6e433ff2052e6917ea92a62d745e15091c9dd9d3
dynamo134 <shaharyaramu16@gmail.com>
2024-07-29 16:41:40 +0530

    let var const

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,16 +1,20 @@
-const accountId = 1234
-let accountEmail = "shaharyar@gmail.com"  //solved problem of No Scope
-var accountPassword = "1234"  //NO Scope Problem 
-accountcity = "Aligarh"
-let accountState;   //Not Assign any Value
+const accountId = 1234   //cannot change the value of accountId just becz of const variable
 // accountId=12345    //cannt change const variable   //Not Allowed
+
+var accountPassword = "1234"  //NO Scope Problem,  koi scope nhi hota h var ka
+
+let accountEmail = "shaharyar@gmail.com"  //solved problem of No Scope
+ 
+accountcity = "Aligarh"
+let accountState;   //Not Assign any Value (undefined)
+
 
 /*
     prefer to use let instead of var
 */
 
-console.log(accountId);
 accountEmail = "shaharyaramu@gmail.com"
 accountcity = "Noida"
 
-console.table([accountId,accountEmail,accountPassword,accountcity,accountState]); 
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState]);
+//[1234,shaharyaramu@gmail.com,1234,Noida,undefined]
